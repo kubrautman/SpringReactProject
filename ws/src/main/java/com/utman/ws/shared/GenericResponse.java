edbb0ef54d7dc6e88fc73dd04dcdc26768301b1d
kubrautman <kubrautman@hotmail.com>
2021-02-22 20:49:42 +0300

    Added Sign up page

---
+++ ws/src/main/java/com/utman/ws/shared/GenericResponse.java
@@ -0,0 +1,15 @@
+package com.utman.ws.shared;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class GenericResponse {
+
+
+	private String message;
+
+
+	
+}
--- ws/src/main/java/com/utman/ws/user/UserController.java
+++ ws/src/main/java/com/utman/ws/user/UserController.java
@@ -1,23 +1,23 @@
 package com.utman.ws.user;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.utman.ws.shared.GenericResponse;
+
 @RestController
 public class UserController {
 	
-	private static final Logger log = LoggerFactory.getLogger(UserController.class);
 	
 	@Autowired
-	UserRepository userRepository ;
+	UserService userService ;
 	
 	@PostMapping("api/1.0/users")
-	public void createUser(@RequestBody User user) {
-		userRepository.save(user);
+	public GenericResponse createUser(@RequestBody User user) {
+		userService.save(user);
+		return new GenericResponse("user created");
 	}
 	
 	
